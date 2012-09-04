93471fba577ae491748c530c9760532312c99292
Anaminus <anaminus@no-email>
2012-09-04 16:51:30 -0500

    tools are now referenced by their data table instead of their index; made the action of selecting a tool independant from the service status

--- source/ToolManager.lua
+++ source/ToolManager.lua
@@ -13,46 +13,62 @@ API:
 local ToolManager do
 	ToolManager = {
 		ToolList = {};
-		CurrentTool = 1;
+		CurrentTool = nil;
 	}
 
 	local eventToolSelected = CreateSignal(ToolManager,'ToolSelected')
 	local eventToolDeselected = CreateSignal(ToolManager,'ToolDeselected')
 
-	function ToolManager:AddTool(data)
-		--[[ data:
-				Name = string
-				Icon = Content
-				ToolTip = string
-				Shortcut = string
-				Select = function
-				Deselect = function
+	function ToolManager:AddTool(tool)
+		--[[ tool:
+				Name		the tool's name
+				Icon		the icon to display on the tool button
+				ToolTip		the text to display when the button is hovered over
+				Shortcut	a shortcut key that selects the tool
+				Select		called when the tool is selected
+				Deselect	called when the tool is deselected
+
+				Button		the button associated with the tool
+				Index		the index of the tool in the manager
 		]]
-		self.ToolList[#self.ToolList+1] = data
+		if not self.Status('Stopped') then
+			error("ToolManager:AddTool: service must be stopped",2)
+		end
+		local index = #self.ToolList+1
+		self.ToolList[index] = tool
+		tool.Index = index
+		if index == 1 then
+			self.CurrentTool = tool
+			eventToolSelected:Fire(tool,false)
+		end
 	end
 
-	function ToolManager:SelectTool(index)
-		local ToolList = self.ToolList
-		index = math.floor(index)
-		if index > 0 and index <= #ToolList then
-			ToolList[self.CurrentTool]:Deselect()
-			eventToolDeselected:Fire(self.CurrentTool)
+	function ToolManager:SelectTool(tool)
+		if tool and tool ~= self.CurrentTool then
+			local started = self.Status('Started')
+			if started then
+				self.CurrentTool:Deselect()
+			end
+			eventToolDeselected:Fire(self.CurrentTool,started)
 
-			self.CurrentTool = index
-			ToolList[index]:Select()
-			eventToolSelected:Fire(index)
-		else
-			error("ToolManager:SelectTool: invalid tool index ["..index.."]",2)
+			self.CurrentTool = tool
+			if started then
+				tool:Select()
+			end
+			eventToolSelected:Fire(tool,started)
 		end
 	end
 
 	AddServiceStatus{ToolManager;
 		Start = function(self)
-			self.ToolList[self.CurrentTool]:Select()
-			eventToolSelected:Fire(self.CurrentTool)
+			if self.CurrentTool then
+				self.CurrentTool:Select()
+			else
+				error("ToolManager must have at least 1 tool",2)
+			end
 		end;
 		Stop = function(self)
-			self.ToolList[self.CurrentTool]:Deselect()
+			self.CurrentTool:Deselect()
 		end;
 	}
 end
