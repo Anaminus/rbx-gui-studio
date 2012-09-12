7d8c9ce1f55af739ea1dfceca8e534eb06b1bc87
Anaminus <anaminus@no-email>
2012-09-12 16:12:24 -0500

    added documentation

--- source/widgets/ButtonMenu.lua
+++ source/widgets/ButtonMenu.lua
@@ -1,3 +1,28 @@
+--[[ButtonMenu
+Creates a menu of clickable buttons from a list of button data.
+Button data are tables that contain the following fields:
+	Name       The name of the button.
+	Icon       An icon to display on the button.
+	ToolTip    (optional) A tooltip to display when the button is hovered over.
+	Select     (optional) A function called when the button is clicked.
+
+If an item in the list is a string, then it is counted as a seperator.
+
+Arguments:
+	buttons
+		The list of button data.
+	size
+		The Vector2 size of each button.
+	horizontal
+		Whether the menu will layout horizontally or vertically.
+	on_click
+		A callback called when any button is clicked.
+		If this argument is defined, then it wil be called instead of button.Select.
+
+Returns:
+	The button menu GUI.
+
+]]
 function Widgets.ButtonMenu(buttons,size,horizontal,on_click)
 	local ButtonMenuFrame = Create'Frame'{
 		Name = "ButtonFrame";
