ed0e21baa7a2ce0f29ebb1160938f0977880be6d
Anaminus <anaminus@no-email>
2012-10-05 14:32:50 -0500

    added snapping settings

--- source/Settings.lua
+++ source/Settings.lua
@@ -33,3 +33,10 @@ end
 -- LayoutMode settings
 CreateEnum'LayoutMode'{'Scale','Offset'}
 Settings.LayoutMode = Enums.LayoutMode(1)
+
+-- Snapping
+Settings.SnapEnabled = false
+Settings.SnapDistance = 8
+Settings.SnapFromCorners = true
+Settings.SnapFromEdges = true
+Settings.SnapFromCenter = true
