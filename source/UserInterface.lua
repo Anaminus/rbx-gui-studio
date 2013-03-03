6433d5b90d6cdf7ee98b6b2af3f17ed60f58c31b
Anaminus <anaminus@no-email>
2013-03-03 16:31:27 -0600

    added snapper button menu

--- source/UserInterface.lua
+++ source/UserInterface.lua
@@ -16,167 +16,289 @@ do
 
 	function UserInterface:Initialize()
 		local GuiColor = InternalSettings.GuiColor
-		local MenuButtons = {
-			{
-				Name = "InsertScreenGui";
-				Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,0,0);
-				ToolTip = "Insert a new ScreenGui";
-				KeyBinding = "shift+ctrl+i";
-				Select = function()
-					ScreenManager:InsertDialog()
-				end;
-			};
-			{
-				Name = "SelectScreenGui";
-				Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,0,1);
-				ToolTip = "Set a ScreenGui to the canvas";
-				KeyBinding = "shift+ctrl+s";
-				Select = function()
-					ScreenManager:SelectDialog()
-				end;
-			};
-			{
-				Name = "ExportScreen";
-				Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,0,6);
-				KeyBinding = "shift+ctrl+e";
-				ToolTip = "Export the current Screen.";
-				Select = function()
-					Exporter:ExportDialog()
-				end;
-			};
-	--[[
-			'----------------';
-			{
-				Name = "Import";
-				Icon = "";
-				ToolTip = "Import a GUI";
-				Select = function()
+		local buttonSize = InternalSettings.GuiButtonSize
+		local menuSize = buttonSize + 8
 
-				end;
-			};
-			{
-				Name = "Export";
-				Icon = "";
-				ToolTip = "Export the current screen";
-				Select = function()
+		local MenuFrame do
+			local MenuButtons = {
+				{
+					Name = "InsertScreenGui";
+					Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,0,0);
+					ToolTip = "Insert a new ScreenGui";
+					KeyBinding = "shift+ctrl+i";
+					Select = function()
+						ScreenManager:InsertDialog()
+					end;
+				};
+				{
+					Name = "SelectScreenGui";
+					Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,0,1);
+					ToolTip = "Set a ScreenGui to the canvas";
+					KeyBinding = "shift+ctrl+s";
+					Select = function()
+						ScreenManager:SelectDialog()
+					end;
+				};
+				{
+					Name = "ExportScreen";
+					Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,0,6);
+					KeyBinding = "shift+ctrl+e";
+					ToolTip = "Export the current Screen.";
+					Select = function()
+						Exporter:ExportDialog()
+					end;
+				};
+		--[[
+				'----------------';
+				{
+					Name = "Import";
+					Icon = "";
+					ToolTip = "Import a GUI";
+					Select = function()
 
-				end;
-			};
-			{
-				Name = "Preview";
-				Icon = "";
-				ToolTip = "Preview the current screen";
-				Select = function()
+					end;
+				};
+				{
+					Name = "Export";
+					Icon = "";
+					ToolTip = "Export the current screen";
+					Select = function()
 
-				end;
-			};
-		--]]
-			'----------------';
-			{
-				Name = "LayoutMode";
-				Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,0,2);
-				ToolTip = "Toggle Layout Mode (currently Scale)";
-				KeyBinding = "alt+l";
-				Select = function(self)
-					if Settings.LayoutMode('Scale') then
-						Settings.LayoutMode = Enums.LayoutMode.Offset
-					else
-						Settings.LayoutMode = Enums.LayoutMode.Scale
-					end
-				end;
-				Setup = function(self)
-					Settings.Changed:connect(function(key,value)
-						if key == 'LayoutMode' then
-							if value('Scale') then
-								Widgets.Icon(self.Button.MenuButtonIcon,InternalSettings.IconMap.Menu,32,0,2)
-								ToolTipService:AddToolTip(self.Button,"Toggle Layout Mode (currently Scale)")
-							else
-								Widgets.Icon(self.Button.MenuButtonIcon,InternalSettings.IconMap.Menu,32,0,3)
-								ToolTipService:AddToolTip(self.Button,"Toggle Layout Mode (currently Offset)")
-							end
-						end
-					end)
-				end;
-			};
-			'----------------';
-			{
-				Name = "ToggleGrid";
-				Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,0,4);
-				ToolTip = "Toggle visibility of the grid";
-				KeyBinding = "alt+g";
-				Select = function(self)
-					Grid:SetVisible(not Grid.Visible)
-				end;
-				Setup = function(self)
-					Grid.VisibilitySet:connect(function(visible)
-						if visible then
-							self.Button.BorderColor3 = GuiColor.ButtonSelected
+					end;
+				};
+				{
+					Name = "Preview";
+					Icon = "";
+					ToolTip = "Preview the current screen";
+					Select = function()
+
+					end;
+				};
+			--]]
+				'----------------';
+				{
+					Name = "LayoutMode";
+					Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,0,2);
+					ToolTip = "Toggle Layout Mode (currently Scale)";
+					KeyBinding = "alt+l";
+					Select = function(self)
+						if Settings.LayoutMode('Scale') then
+							Settings.LayoutMode = Enums.LayoutMode.Offset
 						else
-							self.Button.BorderColor3 = GuiColor.ButtonBorder
+							Settings.LayoutMode = Enums.LayoutMode.Scale
 						end
-					end)
-				end;
-			};
-			{
-				Name = "ToggleSnapping";
-				Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,1,0);
-				ToolTip = "Toggle whether objects will snap to the grid";
-				KeyBinding = "alt+s";
-				Select = function(self)
-					Settings.SnapEnabled = not Settings.SnapEnabled
-				end;
-				Setup = function(self)
-					Settings.Changed:connect(function(key,value)
-						if key == 'SnapEnabled' then
-							if value then
+					end;
+					Setup = function(self)
+						Settings.Changed:connect(function(key,value)
+							if key == 'LayoutMode' then
+								if value('Scale') then
+									Widgets.Icon(self.Button.MenuButtonIcon,InternalSettings.IconMap.Menu,32,0,2)
+									ToolTipService:AddToolTip(self.Button,"Toggle Layout Mode (currently Scale)")
+								else
+									Widgets.Icon(self.Button.MenuButtonIcon,InternalSettings.IconMap.Menu,32,0,3)
+									ToolTipService:AddToolTip(self.Button,"Toggle Layout Mode (currently Offset)")
+								end
+							end
+						end)
+					end;
+				};
+				'----------------';
+				{
+					Name = "ToggleGrid";
+					Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,0,4);
+					ToolTip = "Toggle visibility of the grid";
+					KeyBinding = "alt+g";
+					Select = function(self)
+						Grid:SetVisible(not Grid.Visible)
+					end;
+					Setup = function(self)
+						Grid.VisibilitySet:connect(function(visible)
+							if visible then
 								self.Button.BorderColor3 = GuiColor.ButtonSelected
 							else
 								self.Button.BorderColor3 = GuiColor.ButtonBorder
 							end
-						end
-					end)
-				end;
-			};
-			{
-				Name = "ConfigGrid";
-				Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,1,1);
-				KeyBinding = "shift+ctrl+g";
-				ToolTip = "Configure the grid";
-				Select = function()
-					Grid:ConfigDialog()
-				end;
-			};
-		--[[
-			'----------------';
-			{
-				Name = "ToggleBackground";
-				Icon = "";
-				ToolTip = "Toggle the visiblity of the canvas background";
-				Select = function()
+						end)
+					end;
+				};
+				{
+					Name = "ToggleSnapping";
+					Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,1,0);
+					ToolTip = "Toggle whether objects will snap to the grid";
+					KeyBinding = "alt+s";
+					Select = function(self)
+						Settings.SnapEnabled = not Settings.SnapEnabled
+					end;
+					Setup = function(self)
+						Settings.Changed:connect(function(key,value)
+							if key == 'SnapEnabled' then
+								if value then
+									self.Button.BorderColor3 = GuiColor.ButtonSelected
+								else
+									self.Button.BorderColor3 = GuiColor.ButtonBorder
+								end
+							end
+						end)
+					end;
+				};
+				{
+					Name = "ConfigGrid";
+					Icon = Widgets.Icon(nil,InternalSettings.IconMap.Menu,32,1,1);
+					KeyBinding = "shift+ctrl+g";
+					ToolTip = "Configure the grid";
+					Select = function()
+						Grid:ConfigDialog()
+					end;
+				};
+			--[[
+				'----------------';
+				{
+					Name = "ToggleBackground";
+					Icon = "";
+					ToolTip = "Toggle the visiblity of the canvas background";
+					Select = function()
 
-				end;
-			};
-			{
-				Name = "SetBackgoundColor";
-				Icon = "";
-				ToolTip = "Set the color of the canvas background";
-				Select = function()
+					end;
+				};
+				{
+					Name = "SetBackgoundColor";
+					Icon = "";
+					ToolTip = "Set the color of the canvas background";
+					Select = function()
 
-				end;
-			};
-	--]]
-		}
+					end;
+				};
+		--]]
+			}
 
-		local buttonSize = InternalSettings.GuiButtonSize
-		local menuSize = buttonSize + 8
-
-		local MenuFrame = Widgets.ButtonMenu(MenuButtons,Vector2.new(buttonSize,buttonSize),true)
-		for i,button in pairs(MenuButtons) do
-			if button.KeyBinding then
-				KeyBinding:Add(button.KeyBinding,function() button:Select() end)
+			MenuFrame = Widgets.ButtonMenu(MenuButtons,Vector2.new(buttonSize,buttonSize),true)
+			for i,button in pairs(MenuButtons) do
+				if button.KeyBinding then
+					KeyBinding:Add(button.KeyBinding,function() button:Select() end)
+				end
+				if button.Setup then
+					button:Setup()
+				end
 			end
-			if button.Setup then
-				button:Setup()
+		end
+
+		local SnapperFrame do
+			local SnapperButtons = {
+				{
+					Name = "ToggleSnapping";
+					ToolTip = "Toggle whether objects will snap to anything";
+					Select = function()
+						Settings.SnapEnabled = not Settings.SnapEnabled
+					end;
+					Setup = function(self)
+						Settings.Changed:connect(function(key,value)
+							if key == 'SnapEnabled' then
+								self:SetActive(value)
+							end
+						end)
+						self:SetActive(Settings.SnapEnabled)
+					end;
+				};
+				{
+					Name = "ToggleEdgeSnapping";
+					ToolTip = "Toggle whether objects will snap to the edges of sibling objects";
+					Select = function()
+						Settings.SnapToEdges = not Settings.SnapToEdges
+					end;
+					Setup = function(self)
+						Settings.Changed:connect(function(key,value)
+							if key == 'SnapToEdges' then
+								self:SetActive(value)
+							elseif key == 'SnapEnabled' then
+								self:SetEnabled(value)
+							end
+						end)
+						self:SetActive(Settings.SnapToEdges)
+						self:SetEnabled(Settings.SnapEnabled)
+					end;
+				};
+				{
+					Name = "ToggleCenterSnapping";
+					ToolTip = "Toggle whether objects will snap to the centers of sibling objects";
+					Select = function()
+						Settings.SnapToCenter = not Settings.SnapToCenter
+					end;
+					Setup = function(self)
+						Settings.Changed:connect(function(key,value)
+							if key == 'SnapToCenter' then
+								self:SetActive(value)
+							elseif key == 'SnapEnabled' then
+								self:SetEnabled(value)
+							end
+						end)
+						self:SetActive(Settings.SnapToCenter)
+						self:SetEnabled(Settings.SnapEnabled)
+					end;
+				};
+				{
+					Name = "ToggleParentSnapping";
+					ToolTip = "Toggle whether objects will snap to the parent object";
+					Select = function()
+						Settings.SnapToParent = not Settings.SnapToParent
+					end;
+					Setup = function(self)
+						Settings.Changed:connect(function(key,value)
+							if key == 'SnapToParent' then
+								self:SetActive(value)
+							elseif key == 'SnapEnabled' then
+								self:SetEnabled(value)
+							end
+						end)
+						self:SetActive(Settings.SnapToParent)
+						self:SetEnabled(Settings.SnapEnabled)
+					end;
+				};
+				{
+					Name = "TogglePaddingSnapping";
+					ToolTip = "Toggle whether objects will snap to edges of objects with padding";
+					Select = function()
+						Settings.SnapToPadding = not Settings.SnapToPadding
+					end;
+					Setup = function(self)
+						Settings.Changed:connect(function(key,value)
+							if key == 'SnapToPadding' then
+								self:SetActive(value)
+							elseif key == 'SnapEnabled' then
+								self:SetEnabled(value)
+							end
+						end)
+						self:SetActive(Settings.SnapToPadding)
+						self:SetEnabled(Settings.SnapEnabled)
+					end;
+				};
+				{
+					Name = "ToggleGridSnapping";
+					ToolTip = "Toggle whether objects will snap to the grid";
+					Select = function()
+						Settings.SnapToGrid = not Settings.SnapToGrid
+					end;
+					Setup = function(self)
+						Settings.Changed:connect(function(key,value)
+							if key == 'SnapToGrid' then
+								self:SetActive(value)
+							elseif key == 'SnapEnabled' then
+								self:SetEnabled(value)
+							end
+						end)
+						self:SetActive(Settings.SnapToGrid)
+						self:SetEnabled(Settings.SnapEnabled)
+					end;
+				};
+			}
+
+			SnapperFrame = Widgets.ButtonMenu(SnapperButtons,Vector2.new(buttonSize,buttonSize))
+			for i,button in pairs(SnapperButtons) do
+				if button.KeyBinding then
+					KeyBinding:Add(button.KeyBinding,function() button:Select() end)
+				end
+				if button.Setup then
+					button:Setup()
+				end
 			end
 		end
 
@@ -190,14 +312,14 @@ do
 				Position = UDim2.new(0, 0, 0, -1);
 				BackgroundTransparency = 1;
 				Create'Frame'{
-					Size = UDim2.new(1, -menuSize, 1, -menuSize*2);
+					Size = UDim2.new(1, -menuSize*2, 1, -menuSize*2);
 					BorderSizePixel = 0;
 					Name = "Background";
 					Position = UDim2.new(0, menuSize, 0, menuSize*2);
 					BackgroundColor3 = Color3.new(0.5, 0.5, 0.5);
 				};
 				Create(Canvas.CanvasFrame){
-					Size = UDim2.new(1, -menuSize, 1, -menuSize*2);
+					Size = UDim2.new(1, -menuSize*2, 1, -menuSize*2);
 					Name = "Canvas";
 					Position = UDim2.new(0, menuSize, 0, menuSize*2);
 					BackgroundTransparency = 1;
@@ -205,6 +327,11 @@ do
 				Create(MenuFrame){
 					Name = "MainMenu ButtonMenu";
 					Size = UDim2.new(1,0,0,menuSize);
+				};
+				Create(SnapperFrame){
+					Name = "SnapperMenu ButtonMenu";
+					Position = UDim2.new(1,-menuSize,0,menuSize*2);
+					Size = UDim2.new(0,menuSize,1,0);
 				};
 				Create(ToolbarFrame){
 					Name = "Toolbar ButtonMenu";
