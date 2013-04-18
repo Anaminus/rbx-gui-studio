aa5327f979ffc24846e5f2823dac6e27ec3331e7
Anaminus <anaminus@no-email>
2013-04-18 10:44:39 -0500

    setting a Maid task to nil now disconnects it if the task is an event

--- source/Utility.lua
+++ source/Utility.lua
@@ -237,46 +237,65 @@ Manages the cleaning of events and other things.
 API:
 	Maid[key] = (function)            Adds a task to perform when cleaning up.
 	Maid[key] = (event connection)    Manages an event connection. Anything that isn't a function is assumed to be this.
+	Maid[key] = nil                   Removes a named task. If the task is an event, it is disconnected.
 
 	Maid:GiveTask(task)               Same as above, but uses an incremented number as a key.
 	Maid:Disconnect(...)              Disconnects one or more events.
 	Maid:DoCleaning()                 Disconnects all managed events and performs all clean-up tasks.
 ]]
 do
+	local index = {
+		GiveTask = function(self,task)
+			local n = #self.Tasks+1
+			self.Tasks[n] = task
+			return n
+		end;
+		Disconnect = function(self,...)
+			local tasks = self.Tasks
+			for _,name in pairs{...} do
+				t = type(tasks[name])
+				if t ~= 'nil' and t ~= 'function' then
+					tasks[name]:disconnect()
+					tasks[name] = nil
+				end
+			end
+		end;
+		DoCleaning = function(self)
+			local tasks = self.Tasks
+			for name,task in pairs(tasks) do
+				if type(task) == 'function' then
+					task()
+				else
+					task:disconnect()
+				end
+				tasks[name] = nil
+			end
+		end;
+	};
 	local mt = {
-		__index = {
-			GiveTask = function(self,task)
-				local n = #self+1
-				self[n] = task
-				return n
-			end;
-			Disconnect = function(self,...)
-				for _,name in pairs{...} do
-					t = type(self[name])
-					if t ~= 'nil' and t ~= 'function' then
-						self[name]:disconnect()
-						self[name] = nil
-					end
+		__index = function(self,k)
+			if index[k] then
+				return index[k]
+			else
+				return self.Tasks[k]
+			end
+		end;
+		__newindex = function(self,k,v)
+			local tasks = self.Tasks
+			if v == nil then
+				-- disconnect if the task is an event
+				if type(tasks[k]) ~= 'function' then
+					tasks[k]:disconnect()
 				end
-			end;
-			DoCleaning = function(self)
-				for name,task in pairs(self) do
-					if type(task) ~= 'function' then
-						task:disconnect()
-						self[name] = nil
-					end
-				end
-				for name,task in pairs(self) do
-					if type(task) == 'function' then
-						task()
-						self[name] = nil
-					end
-				end
-			end;
-		};
+			elseif tasks[k] then
+				-- clear previous task
+				self[k] = nil
+			end
+			tasks[k] = v
+		end;
 	}
 	function CreateMaid()
-		return setmetatable({},mt)
+		return setmetatable({Tasks={}},mt)
 	end
 end
 
